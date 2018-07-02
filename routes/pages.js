fab40b1b483f510bb12f94c31447090adbea132b
wesleyshynes <wesleyshynes@gmail.com>
2018-07-02 12:29:43 -0400

    create-page route added, when posted to updates data.json with new info

--- routes/pages.js
+++ routes/pages.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 /* GET users listing. */
 router.get('/:pageHandle', function(req, res, next) {
 	//console.log(pageContent[req.params.pageHandle])
-	data = JSON.parse(fs.readFileSync('D://node/mconServer/public/data/pages.json','utf8'))
+	let data = JSON.parse(fs.readFileSync('D://node/mconServer/public/data/pages.json','utf8'))
 	//console.log(data)
 	if(data[req.params.pageHandle]){
 		res.send(data[req.params.pageHandle]);
@@ -13,5 +13,13 @@ router.get('/:pageHandle', function(req, res, next) {
 		res.send(JSON.stringify(data['404']));
 	}  
 });
+
+router.post('/create-page',(req,res,next)=>{
+	let data = JSON.parse(fs.readFileSync('D://node/mconServer/public/data/pages.json','utf8'))
+	console.log(req.body)
+	data[req.body.handle] = {...req.body}
+	fs.writeFileSync('D://node/mconServer/public/data/pages.json', JSON.stringify(data));
+	res.send('page created/updated')
+})
 
 module.exports = router;
